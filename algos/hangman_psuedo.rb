7c10021f82e60da3156bb77959e0899300eb00a2
Kilima Glenn <thunderenlight@gmail.com>
2016-07-14 22:50:37 -0500

    adding slow brute force kata

--- algos/hangman_psuedo.rb
+++ algos/hangman_psuedo.rb
@@ -9,3 +9,18 @@ x = 0
 	
 	Prime::Generator23.new
 end
+
+def highest_biPrimefac(p1, p2, n)
+  result = []
+  d = p1* p2 * 2
+  (n).downto(d).detect do |n|
+    if n.prime_division.length ==2
+      if  n.prime_division[0][0] == p1 && n.prime_division[1][0] == p2 
+       result << n
+       result << n.prime_division[0][1]
+       result << n.prime_division[1][1]
+      end
+    end
+   end
+ return result
+end
