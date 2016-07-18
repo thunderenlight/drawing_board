91ac42fb66f22b42b443b665d2e4d402982fc61d
Kilima Glenn <thunderenlight@gmail.com>
2016-07-17 23:35:46 -0500

    adding slow brute force kata prerefactor

--- algos/hangman_psuedo.rb
+++ algos/hangman_psuedo.rb
@@ -24,3 +24,40 @@ def highest_biPrimefac(p1, p2, n)
    end
  return result
 end
+
+#def highest_biPrimefac(p1, p2, n)
+#     # your code biggest num, smaller nums exp, larger num exp
+#     #until max >= n, (p1 ** (range)) * (p2 **(range))
+#     # p1 ** 1..? * p2 ** 1..? 
+#     # if p2 **exp > max
+#     #   p1 ** exp
+#     exp1 = 1
+#     exp2 = 1
+#     max = p1 * p2
+#      until  p1 * p2 >= n
+#     	# exp1 += 1
+
+#     	# if p1 * p2  < n &&  p1 * p2  > p1  * p2  ** (exp1 -1)
+#     	if p1 * p2 ** (exp2 +1) > max &&  p1 * p2 ** (exp2 +1) <= n
+#     		exp2 +=1
+#     		p p2 = p2 ** exp2
+
+#     		puts max 
+#     		puts "hiya"
+    	
+#     	elsif p2 * p1 ** (exp1 +1) > max && p2 * p1 ** (exp1 +1) <= n
+
+#     		p "P1"
+#     		exp1 += 1
+#     		p p1 = p1 ** exp1
+#     		max = p1 **( exp1 + 1 )* p2 ** exp2
+#     		# break
+#     	elsif p1 ** (exp1 +1) * p2 ** (exp2 -1)  > p1 ** (exp1 -1) * p2 ** (exp2 +1)  && p2 * p1 ** (exp1 +1) <= n  && p2 * p1 ** (exp1 +1) > max
+#     		p "P2"
+#     		p  max = p1 **( exp1 + 1 )* p2 ** exp2
+#     		exp2 -= 1
+#     	end
+#     end
+#     puts max
+
+# end
