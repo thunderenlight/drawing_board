92bf817c06eff3d9584d1ecc6072709a69a3b174
thunderenlight <thunderenlight@gmail.com>
2016-07-27 19:41:45 -0500

    adding play pass tests

--- algos/hangman_psuedo.rb
+++ algos/hangman_psuedo.rb
@@ -110,3 +110,14 @@ Test.describe("make_valley") do
     testing(make_valley([19, 8, 6]), [19, 6, 8])
   end
 end
+
+
+
+def play_pass(str, n)
+    #your code
+end
+
+Test.assert_equals(play_pass("I LOVE YOU!!!", 1), "!!!vPz fWpM J")
+
+Test.assert_equals(play_pass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2), 
+    "4897 NkTrC Hq fT67 GjV Pq aP OqTh gOcE CoPcTi aO")
